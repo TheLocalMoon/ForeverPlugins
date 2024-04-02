d8f65e9ea473e59293a9d6b318b815431be4156c
TheLocalMoon <134215072+TheLocalMoon@users.noreply.github.com>
2024-04-02 15:34:46 +0300

    Add files via upload

---
+++ plugins/devil.py
@@ -0,0 +1,36 @@
+# devil.py #
+import sys
+sys.path.append('..')
+from pdk import *
+on = True
+
+print("""Beyond the tree
+Fire burns
+Secret love
+Bloodline yearns
+Dark minds embrace
+Crimson joy
+Does your
+Dim heart
+Heal or
+Destroy?
+""")
+
+if on:
+    show_info("devil.py", "Beyond")
+    show_info("devil.py", "The tree")
+    show_info("devil.py", "Fire")
+    show_info("devil.py", "Burns")
+    show_info("devil.py", "Secret")
+    show_info("devil.py", "Love")
+    show_info("devil.py", "Bloodline")
+    show_info("devil.py", "Yearns")
+    show_info("devil.py", "Dark minds")
+    show_info("devil.py", "Embrace")
+    show_info("devil.py", "Crimson")
+    show_info("devil.py", "Joy")
+    show_info("devil.py", "Does your")
+    show_info("devil.py", "Dim heart")
+    show_info("devil.py", "Heal or")
+    show_info("devil.py", "Destroy?")
+else: pass
